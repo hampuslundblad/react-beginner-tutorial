933d2002291e87ab4eb07e7f314209e9642f9d40
Hampus Lundblad <hampus.lundblad@hotmail.se>
2024-08-08 16:22:39 +0200

    step 9 done

--- src/pages/Lists.tsx
+++ src/pages/Lists.tsx
@@ -1,17 +1,19 @@
+import { useState } from "react";
 import Title from "../components/Title";
 import UnorderedList, { Ingredient } from "../components/UnorderedList";
 
 function Lists() {
-  const ingredients: Ingredient[] = [
-    {
-      name: "milk",
-      amount: "ll",
-    },
-  ];
+  const [ingredients, setIngredients] = useState<Ingredient[]>([])
+ 
+  function handleClick() {
+    setIngredients((previousValue => [...previousValue, {name:"wheat", amount:"ll"}]))
+    console.log(ingredients);
+  }
   return (
     <div>
       <Title text="Hello Lists" />
       <UnorderedList list={ingredients} />
+      <button onClick={handleClick}>Add item to list</button>
     </div>
   );
 }
